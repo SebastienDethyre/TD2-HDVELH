e585eacc754c22f027563fb55e6854b5dd52b2c0
Sebastien Dethyre <sebastien.dethyre@etu.univ-amu.fr>
2022-01-22 22:57:11 +0100

    Poursuite d'implémentation d'EventRandomSolution

--- src/pracHDVELH/EventRandomSolution.java
+++ src/pracHDVELH/EventRandomSolution.java
@@ -1,15 +1,27 @@
 package pracHDVELH;
+import java.util.Random;
 
 public class EventRandomSolution extends Event {
-
+	private static final int ERROR_STATUS_BAD_SETTINGS =-1;
+	private static final int DEFAULT_RANDOM_SOLUTION = 0;
+	private static final String ERROR_MSG_BAD_SETTINGS = "Bad settings";
+	private int dice;
+	private int randomSolution;
+	private Random randomGenerator;
+	private int [] partition; 
+	private String waitingMsg;
+	private String solutionAnnoucement;
+	
 	public EventRandomSolution() {
 		this (new GUIManager(),null, new int[]{1,2,3,4,5,6}, null, null);
 	}
 
-	public EventRandomSolution(GUIManager gui, String data, int[] mask, String string2, String string3) {
+	public EventRandomSolution(GUIManager gui, String data, int[] partition, String waitingMsg, String solutionAnnoucement) {
 		super(gui,data);
-		
-
+		this.randomGenerator = new Random();
+		this.partition = partition;
+		this.waitingMsg = waitingMsg;
+		this.solutionAnnoucement = solutionAnnoucement;
 	}
 
 }
